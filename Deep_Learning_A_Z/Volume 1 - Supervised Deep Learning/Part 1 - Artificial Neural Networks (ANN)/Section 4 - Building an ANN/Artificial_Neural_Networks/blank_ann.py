77b87d3367bde1276fbb8d24fca4c0511afd1bab
Charlie <thecharlie.caii@gmail.com>
2019-11-23 12:35:33 -0500

    Completed grid search, above 86% accuracy model

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/Artificial_Neural_Networks/blank_ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/Artificial_Neural_Networks/blank_ann.py
@@ -33,15 +33,18 @@ X_test = sc.transform(X_test)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 
 # Initializing NN
 classifier = Sequential()
 
 # Adding input layer and first hidden layer
 classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
+classifier.add(Dropout(rate=0.1))
 
 # Add second hidden layer
 classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+classifier.add(Dropout(rate=0.1))
 
 # Add output layer
 classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
@@ -66,3 +69,67 @@ acc = (1510 + 205) / 2000
 # Single prediction
 new_prediction = classifier.predict(sc.transform(np.array([[0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
 new_prediction = (new_prediction > 0.5)
+
+# Evaluating the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+
+classifier = KerasClassifier(build_fn=build_classifier, batch_size=10, epochs=100)
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=-1)
+mean = accuracies.mean()
+variance = accuracies.std()
+
+# Tuning ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator=classifier,
+                           param_grid=parameters,
+                           scoring='accuracy',
+                           cv=10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+# Above 86% accuracy
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units=12, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
+    classifier.add(Dense(units=12, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+
+classifier = KerasClassifier(build_fn=build_classifier, batch_size=25, epochs=510)
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=-1)
+mean = accuracies.mean()
+variance = accuracies.std()
