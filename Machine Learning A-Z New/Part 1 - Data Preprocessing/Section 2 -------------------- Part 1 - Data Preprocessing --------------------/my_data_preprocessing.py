457e952ec1c6ff548749ed844d645cd12a972d26
Charlie <thecharlie.caii@gmail.com>
2019-12-08 16:47:32 -0500

    Updated data preprocessing templates

--- Machine Learning A-Z New/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/my_data_preprocessing.py
+++ Machine Learning A-Z New/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/my_data_preprocessing.py
@@ -11,28 +11,28 @@ dataset = pd.read_csv(
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Missing data
-from sklearn.impute import SimpleImputer
-
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean', verbose=0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-# Encode categorical data
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.compose import ColumnTransformer
-
-ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(ct.fit_transform(X), dtype=np.float)
-
-y = LabelEncoder().fit_transform(y)
+# # Missing data
+# from sklearn.impute import SimpleImputer
+#
+# imputer = SimpleImputer(missing_values=np.nan, strategy='mean', verbose=0)
+# imputer = imputer.fit(X[:, 1:3])
+# X[:, 1:3] = imputer.transform(X[:, 1:3])
+#
+# # Encode categorical data
+# from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+# from sklearn.compose import ColumnTransformer
+#
+# ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+# X = np.array(ct.fit_transform(X), dtype=np.float)
+#
+# y = LabelEncoder().fit_transform(y)
 
 # Splitting dataset into Training and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+# # Feature Scaling
+# from sklearn.preprocessing import StandardScaler
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.fit_transform(X_test)
