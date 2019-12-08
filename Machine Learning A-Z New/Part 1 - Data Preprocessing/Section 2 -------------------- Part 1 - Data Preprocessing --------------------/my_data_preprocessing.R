516aba5cb071f56203a393748ddb4621a76d13d4
Charlie <thecharlie.caii@gmail.com>
2019-12-08 16:47:47 -0500

    Completed in R

--- Machine Learning A-Z New/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/my_data_preprocessing.R
+++ Machine Learning A-Z New/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/my_data_preprocessing.R
@@ -1,21 +1,34 @@
 # Data Preprocessing
 
 # Import dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('~/Desktop/DeepLearning/Machine Learning A-Z New/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data.csv')
+dataset = dataset[, 2:3]
+# # Missing data
+# dataset$Age = ifelse(is.na(dataset$Age), 
+#                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+#                      dataset$Age)
+# 
+# dataset$Salary = ifelse(is.na(dataset$Salary), 
+#                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+#                         dataset$Salary)
+# 
+# # Encode categorical data
+# dataset$Country = factor(dataset$Country, 
+#                          levels = c('France', 'Spain', 'Germany'),
+#                          labels = c(1, 2, 3))
+# dataset$Purchased = factor(dataset$Purchased, 
+#                            levels = c('No', 'Yes'),
+#                            labels = c(0, 1))
 
-# Missing data
-dataset$Age = ifelse(is.na(dataset$Age), 
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
+# Splitting dataset into Training and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
-dataset$Salary = ifelse(is.na(dataset$Salary), 
-                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                        dataset$Salary)
+# # Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
 
-# Encode categorical data
-dataset$Country = factor(dataset$Country, 
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased, 
-                           levels = c('No', 'Yes'),
-                           labels = c(0, 1))
