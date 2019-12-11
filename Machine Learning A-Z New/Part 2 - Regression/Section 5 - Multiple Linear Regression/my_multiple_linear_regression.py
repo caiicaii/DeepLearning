8445060c9b9c094e19ea705a2c0d15cf01e17e46
Charlie <thecharlie.caii@gmail.com>
2019-12-10 21:23:12 -0500

    Finished multiple linear regression in Python

--- Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.py
@@ -1,2 +1,33 @@
 # Multiple Linear Regression
 
+# Imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import dataset
+dataset = pd.read_csv('Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.compose import ColumnTransformer
+
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+# Avoiding Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting dataset into Training and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Fitting Multiple Linear Regression to Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting Test set results
+y_pred = regressor.predict(X_test)
