88423ce647dd0a420ca3e1a0261a95e1ceac07b7
Charlie <thecharlie.caii@gmail.com>
2019-12-10 22:33:49 -0500

    Working on Backward Elimination

--- Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.py
@@ -31,3 +31,24 @@ regressor.fit(X_train, y_train)
 
 # Predicting Test set results
 y_pred = regressor.predict(X_test)
+
+# Building a better model (Backward Elimination)
+import statsmodels.api as sm
+X = np.append(np.ones((50, 1)).astype(int), X, axis=1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+
