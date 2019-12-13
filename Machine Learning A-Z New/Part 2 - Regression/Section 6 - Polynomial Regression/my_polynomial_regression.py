90e5d998a7079b7ceb6ca48dd320dd3f5f927d58
Charlie <thecharlie.caii@gmail.com>
2019-12-12 23:16:23 -0500

    Completed linear regression visualization

--- Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
@@ -22,3 +22,11 @@ poly_reg = PolynomialFeatures(degree=2)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
+
+# Visualizing Linear Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title("Truth or Bluff (Linear Regression)")
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
