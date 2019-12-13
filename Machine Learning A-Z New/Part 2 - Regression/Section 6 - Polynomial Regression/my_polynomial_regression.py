13019a2c6b19a75d5e53d08afb2c5effb2d593ab
Charlie <thecharlie.caii@gmail.com>
2019-12-12 23:09:18 -0500

    Fit linear regression

--- Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
@@ -10,3 +10,10 @@ dataset = pd.read_csv(
     'Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+
+# Fitting Linear Regression to dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+
