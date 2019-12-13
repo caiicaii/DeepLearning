67067321354f9f21f3347fb9fda526c351be36d7
Charlie <thecharlie.caii@gmail.com>
2019-12-12 23:13:20 -0500

    Fit polynomial regression

--- Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
@@ -16,4 +16,9 @@ from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-
+# Fitting Polynomial Regression to dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
