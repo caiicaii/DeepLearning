c3796198880b7ab6adc166b99ae2bb26c3390f51
Charlie <thecharlie.caii@gmail.com>
2019-12-12 23:23:50 -0500

    Improved polynomial regression predictions

--- Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
@@ -18,7 +18,7 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=2)
+poly_reg = PolynomialFeatures(degree=5)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
@@ -32,6 +32,8 @@ plt.ylabel('Salary')
 plt.show()
 
 # Visualizing Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
 plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
 plt.title("Truth or Bluff (Linear Regression)")
