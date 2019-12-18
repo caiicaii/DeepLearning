1f5908a00a3de491a0cd279ae10f3cc5188f63d5
Charlie <thecharlie.caii@gmail.com>
2019-12-17 22:01:02 -0500

    Fit both regression models

--- Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.R
+++ Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.R
@@ -6,8 +6,17 @@ dataset = dataset[2:3]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Fitting Linear Regression to dataset
+lin_reg = lm(formula = Salary ~ .,
+             data = dataset)
+
+# Fitting Polynomial Regression to dataset
+dataset$Level2 = dataset$Level^2
+poly_reg = lm(formula = Salary ~ .,
+              data = dataset)
