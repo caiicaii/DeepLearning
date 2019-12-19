8393e847c56ca045a5adbaa553191954baacd881
Charlie <thecharlie.caii@gmail.com>
2019-12-18 21:55:04 -0500

    Added SVR in R

---
+++ Machine Learning A-Z New/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/my_svr.R
@@ -0,0 +1,46 @@
+# Support Vector Regression
+
+# Importing dataset
+dataset = read.csv('~/Desktop/DeepLearning/Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Fitting SVR to dataset
+# install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression')
+
+# Predicting new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualizing SVR results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualizing SVR results (higher detail)
+# library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
